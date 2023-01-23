d27f1a476bab5adc45c1e189f9db660292cfd3a6
user <user@WIN-QPM356H66DK>
2023-01-23 15:50:41 +0600

    Add project files.

---
+++ ConvertFloatPrice/Program.cs
@@ -0,0 +1,10 @@
+﻿float price = 100.2f;
+object objPrice = price;
+int newPrice = (int)(float)objPrice;
+// int newPrice = (int)objPrice; 
+//// Exception: Unable to cast object of type System.Single to type System.Int32
+// int newPrice = (int)(double)objPrice; 
+//// Exception: Unable to cast object of type System.Single to type System.Int32
+// int newPrice = objPrice; 
+//// Cannot implicity(неявно) convert type object to int. An explicit converstion is exist, are you missing a cast(тип)?
+Console.WriteLine(newPrice); 
